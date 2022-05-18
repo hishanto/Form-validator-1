fde0dddbb99f87dfd7f0f7284bc2c169d8ec2658
hishanto <husain.imambd@gmail.com>
2022-05-18 13:39:33 +0600

    js match passwrod and min, max checker | css cointainer width 50 percent

--- script.js
+++ script.js
@@ -31,9 +31,30 @@ function getCapFirstLetter(errorID) {
     return errorID.id.charAt(0).toUpperCase() + errorID.id.slice(1);
 
 }
+//check the length of userName and password
+function checkLength(input, min, max) {
+    if(input.value.length < min){
+        showError(input, `${getCapFirstLetter(input)} need to be greater than ${min} character`)
+    }
+    else if(input.value.length > max){
+        showError(input, `${getCapFirstLetter(input)} need to be less than ${max} character`);
+    }
+    else{
+        showSuccess(input);
+    }
+}
+// match password
+function MatchPasswordCheck(input1, input2) {
+    if (input1 !== input2) {
+        showError(input2, `Password did not match`);
+    }
+}
 form.addEventListener('submit', function(e){
     e.preventDefault();
      
     checkRequired([username,email,password,password2]);
     // console.log(username.value);
+    checkLength(username, 3, 15) ;
+    checkLength(password, 6, 25) ;
+    MatchPasswordCheck(password, password2)
 });
